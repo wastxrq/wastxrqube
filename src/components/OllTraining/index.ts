ded2e0bc5b5abf29b2e81bae888fe20ac5142bd7
Andrew Kononenko <wastardy.k@gmail.com>
2026-08-21 20:51:58 +0300

    feat(components): add OllCaseStats component for displaying training statistics

---
+++ src/components/OllTraining/index.ts
@@ -0,0 +1,3 @@
+export { default as OllCaseDiagram } from './OllCaseDiagram.vue'
+export { default as OllCaseSelector } from './OllCaseSelector.vue'
+export { default as OllCaseStats } from './OllCaseStats.vue'
