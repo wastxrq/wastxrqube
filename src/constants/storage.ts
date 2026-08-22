ed3fde159fc8f9c51c3e4b64d49e16294f9f1a9b
Andrew Kononenko <wastardy.k@gmail.com>
2026-08-22 21:46:18 +0300

    feat(constants): add pll selection and history storage keys with limit

--- src/constants/storage.ts
+++ src/constants/storage.ts
@@ -1,7 +1,11 @@
 export const OLL_SELECTION_STORAGE_KEY = 'wastxrqube:ollSelection'
 export const OLL_HISTORY_STORAGE_KEY = 'wastxrqube:ollHistory'
 
+export const PLL_SELECTION_STORAGE_KEY = 'wastxrqube:pllSelection'
+export const PLL_HISTORY_STORAGE_KEY = 'wastxrqube:pllHistory'
+
 /** Oldest entries are dropped once history exceeds this many attempts. */
 export const OLL_HISTORY_LIMIT = 1000
+export const PLL_HISTORY_LIMIT = 1000
 
 export const TIMER_SESSIONS_STORAGE_KEY = 'wastxrqube:timerSessions'
